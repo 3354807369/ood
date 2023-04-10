9206b0c1ddd2e4e170fa77d01c4355c052a55b85
dongchen xie <3354807369@qq.com>
2023-04-10 21:03:57 +0930

    a

--- main.cpp
+++ main.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
+#include "QuickSort.h"
+#include "RecursiveBinarySearch.h"
 
+int main() {
+  std::string input;
+  std::getline(std::cin, input);
+  std::istringstream iss(input);
+  std::vector<int> list;
+  int num;
+  while (iss >> num) {
+    list.push_back(num);
+  }
+  QuickSort qs;
+  RecursiveBinarySearch rbs;
+  list = qs.sort(list);
+  bool isFound = rbs.search(list, 1);
+  std::cout << (isFound ? "true" : "false");
+  for (int i = 0; i < list.size(); ++i) {
+    std::cout << " " << list[i];
+  }
+  std::cout << std::endl;
+  return 0;
+}
