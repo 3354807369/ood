0d6bd902a8a5655ba9d7c8fc97b31031df809e9d
dongchen xie <3354807369@qq.com>
2023-04-10 20:48:38 +0930

    a

--- main.cpp
+++ main.cpp
@@ -1,16 +1,1 @@
-#include "QuickSort.h"
-#include <iostream>
-#include <vector>
 
-int main()
-{
-    std::vector<int> v{5, 3, 8, 1, 9, 2};
-    QuickSort qs;
-    std::vector<int> sorted_v = qs.sort(v);
-    std::cout << "Sorted vector: ";
-    for (int x : sorted_v) {
-        std::cout << x << ' ';
-    }
-    std::cout << '\n';
-    return 0;
-}
