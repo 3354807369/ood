f9555c731f04e031315234ce08c41a33ab62a995
3354807369 <3354807369@qq.com>
2023-05-21 19:45:14 +0930

    a

---
+++ klargest.cpp
@@ -0,0 +1,13 @@
+#include <queue>
+#include <vector>
+using namespace std;
+int kth_largest(vector<int> values, int k){
+    priority_queue<int,vector<int>,greater<int>> pri_queue;
+    for(int i = 0; i<values.size();i++){
+        pri_queue.push(values[i]);
+    }
+    for(int i=1;i<k;i++){
+        pri_queue.pop();
+    }
+    return pri_queue.top();
+}
